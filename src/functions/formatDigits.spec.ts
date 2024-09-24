ef03e01f694edb8bc4b7c947fc111cc93b9a3029
Uri <uri.brodetzki@gmail.com>
2024-09-24 17:17:25 -0400

    Basic operation of formatDigits is working - all tests are passing

--- src/functions/formatDigits.spec.ts
+++ src/functions/formatDigits.spec.ts
@@ -1,7 +1,63 @@
 import formatDigits from './formatDigits';
 
+
 /**
  * formatDigits TESTS
  *
  * @group unit/formatDigits
+ * @group only
  */
+
+
+describe('formatDigits', () => {
+
+  const formatPhone = `(###) ###-####`;
+  const formatThousand = `##,###.##`;
+
+  it('takes a string of digits and fit them in the format', () => {
+    expect(formatDigits('1234567890', formatPhone)).toBe('(123) 456-7890');
+    expect(formatDigits('1234567', formatThousand)).toBe('12,345.67');
+  });
+
+  it('extracts just the digits out of a string that has both digits and other characters', () => {
+    expect(formatDigits('1s23se4567yj-8?90', formatPhone)).toBe('(123) 456-7890');
+    expect(formatDigits('1+23e4=[56]]]7', formatThousand)).toBe('12,345.67');
+  });
+
+  it('returns empty string if the string does not contain any digits as default behavior', () => {
+    expect(formatDigits('ABCDEFG', formatPhone)).toBe('');
+  });
+
+  it('returns the default output when string has no digits and `failedOutput` is set to "empty"', () => {
+    expect(formatDigits('ABCDEFG', formatPhone, { failedOutput: 'empty' })).toBe('');
+  });
+
+  it('returns the original input if the string does not contain digits and the `failedOutput` is set to "original"', () => {
+    expect(formatDigits('ABCDEFG', formatPhone, { failedOutput: 'original' })).toBe('ABCDEFG');
+  });
+
+  it('returns empty string if there are no digits in the string and `failedOutput==="digits"', () => {
+    expect(formatDigits('ABCDEFG', formatPhone, { failedOutput: 'empty' })).toBe('');
+  });
+
+  it('returns incomplete formatted number if there are not enough digits as a default behavior', () => {
+    expect(formatDigits('12345678', formatPhone)).toBe('(123) 456-78');
+    expect(formatDigits('123456', formatThousand)).toBe('12,345.6');
+  });
+
+  it('returns empty string when not enough digits for the format and `incompleteFormat` option is set to `false`', () => {
+    expect(formatDigits('12345678', formatPhone, { incompleteFormat: false })).toBe('');
+  });
+
+  it('returns empty string when there are not enough digits, `incompleteFormat===false` and `failedOutput="empty"`', () => {
+    expect(formatDigits('12345678', formatPhone, { incompleteFormat: false, failedOutput: 'empty' })).toBe('');
+  });
+
+  it('returns the original input if not enough digits, `incompleteFormat===false` and `failedOutput=="original"`', () => {
+    expect(formatDigits('1-2345+67++8', formatPhone, { incompleteFormat: false, failedOutput: 'original' })).toBe('1-2345+67++8');
+  });
+
+  it('returns only the extracted digits if not enough to complete the format, `incompleteFormat===false` and `failedOutput==="digits"`', () => {
+    expect(formatDigits('1-2345+67++8', formatPhone, { incompleteFormat: false, failedOutput: 'digits' })).toBe('12345678');
+  });
+});
--- src/functions/formatDigits.ts
+++ src/functions/formatDigits.ts
@@ -1,5 +1,95 @@
-function formatDigits(input: string) {
+function extractDigits(str: string) {
+  return str.replace(/\D/g, ''); // eslint-disable-line unicorn/prefer-string-replace-all
+}
 
+function amountOfPounds(strWithPounds: string) {
+  return strWithPounds.split('#').length - 1;
+}
+
+
+interface FormatDigitsOptions {
+  failedOutput?: 'empty' | 'original' | 'digits';
+  incompleteFormat?: boolean;
+}
+
+const defaultOptions: Required<FormatDigitsOptions> = {
+  failedOutput: 'empty',
+  incompleteFormat: true
+};
+
+
+/**
+ * Takes a string that should include digit characters (numbers from 0 to 9) and a format, and returns a string of all the digits formatted according to the given format
+ *
+ * ```typescript
+ * expect(formatDigits('1+23abc4567$890==', '(###) ###-####`)).toBe('(123) 456-7890);
+ * ```
+ *
+ * Options:
+ * ========
+ * `incompleteFormat`
+ * ------------------
+ * *Defaults to* `true`
+ * If there are not enough digits in the string to fill up the entire format, when this option is set to `true` it returns the format in incomplete form.
+ * If set to `false` it treats the situation as a failure and then return the failedOutput.
+ *
+ * ```typescript
+ * expect(formatDigits('1234567', `(###) ###-####`, { incompleteFormat: true })).toBe('(123) 456-7');
+ * expect(formatDigits('1234567', `(###) ###-####`, { incompleteFormat: false })).toBe(''); // See the default behavior of failedOutput
+ * ```
+ *
+ * `failedOutput`
+ * ------------------
+ * *Defaults to* "empty".
+ * Dictates what should be returned in case that the formatting of the string failed. Failure happens where there are not enough digits to satisfy the format or there are not digits at all.
+ *
+ * It has three options:
+ * `empty`: returns empty string
+ * `original`: returns the original string from the argument
+ * `digits`: returns only the digits from the argument
+ *
+ * ```typescript
+ * expect(formatDigits('ABCDE', '#####`, { failedOutput: 'empty' })).toBe('');
+ * expect(formatDigits('ABCDE', '#####`, { failedOutput: 'original' })).toBe('ABCDE');
+ * expect(formatDigits('12++3-4', '#####`, { incompleteFormat: false, failedOutput: 'digits' })).toBe('1234');
+ * ```
+ * @param input A string that includes digit characters to be formatted
+ * @param format The format to apply on all the digits
+ * @param options Modifiers for how to apply the format on the string
+ * @returns A string that contains only the digit characters from an input string in a defined format
+ */
+function formatDigits(input: string, format: string, options?: FormatDigitsOptions) {
+
+  const failedOutput = options?.failedOutput || defaultOptions.failedOutput;
+  const incompleteFormat = options?.incompleteFormat ?? defaultOptions.incompleteFormat;
+
+
+  const digits = extractDigits(input);
+
+  function outputOnFailure() {
+    if (failedOutput === 'empty') return '';
+    if (failedOutput === 'original') return input;
+    return digits;
+  }
+
+
+  if (!digits) return outputOnFailure();
+
+  if (digits.length < amountOfPounds(format) && !incompleteFormat) return outputOnFailure();
+
+  let formatted = '';
+  let digitIndex = 0;
+
+  for (const char of format) {
+    if (char === '#') {
+      formatted += digits[digitIndex] || '';
+      digitIndex += 1;
+    } else {
+      formatted += char;
+    }
+  }
+
+  return formatted;
 }
 
 export default formatDigits;

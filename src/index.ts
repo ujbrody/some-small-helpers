0a2689c7742d54369845d62003efd277c44bc378
Uri Brodetzki <uri@qupell.com>
2025-01-28 22:57:20 -0500

    Fix problem with index file

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,5 @@ export { default as formatDigits } from './functions/formatDigits';
 /* Objects */
 
 export { default as flattenValues } from './functions/flattenValues';
+export { default as cleanEmpty } from './functions/cleanEmpty';
+export { default as mapValues } from './functions/mapValues';

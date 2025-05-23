451aca7a0c2dfae00a87f4c8a5bff2342b9c8946
Uri Brodetzki <uri@qupell.com>
2025-05-23 10:54:57 -0400

    Finished building the `probabilityArray` function - all tests are passing

--- src/functions/probabilityArray.spec.ts
+++ src/functions/probabilityArray.spec.ts
@@ -1,4 +1,12 @@
-import probabilityArray from './probabilityArray';
+import probabilityArray, { ProbabilityItem } from './probabilityArray';
+
+
+/**
+ * cleanEmpty TESTS
+ *
+ * @group unit/cleanEmpty
+ */
+
 
 describe('probabilityArray', () => {
 
@@ -56,7 +64,7 @@ describe('probabilityArray', () => {
     expect(boom).toHaveLength(0);
   });
 
-  it('reduces the first argument that complete 100 partially', () => {
+  it('reduces the first argument that complete 100 partially as default', () => {
     const arr = probabilityArray(['foo', 0.3], ['bar', 0.4], ['blah', 0.2], ['boom', 0.3]);
     expect(arr).toHaveLength(100);
 
@@ -71,6 +79,21 @@ describe('probabilityArray', () => {
     expect(boom).toHaveLength(10);
   });
 
+  it('completely eliminates arguments that exceed 100', () => {
+    const arr = probabilityArray(['foo', 0.3], ['bar', 0.4], ['blah', 0.4], ['boom', 0.3]);
+    expect(arr).toHaveLength(100);
+
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+    const boom = arr.filter((item) => item === 'boom');
+
+    expect(foo).toHaveLength(30);
+    expect(bar).toHaveLength(40);
+    expect(blah).toHaveLength(30); // This one was also shortened
+    expect(boom).toHaveLength(0);
+  })
+
   it('returns shorter array if total sum does not come up to 100', () => {
     const arr = probabilityArray(['foo', 0.3], ['bar', 0.4], ['blah', 0.2]);
     expect(arr).toHaveLength(90);
@@ -82,6 +105,257 @@ describe('probabilityArray', () => {
     expect(foo).toHaveLength(30);
     expect(bar).toHaveLength(40);
     expect(blah).toHaveLength(20);
+  });
+
+  it('consolidates arguments that are the same', () => {
+    const arr = probabilityArray(['foo', 0.2], ['bar', 0.6], ['blah', 0.1], ['foo', 0.1]);
+    expect(arr).toHaveLength(100);
+
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(30);
+    expect(bar).toHaveLength(60);
+    expect(blah).toHaveLength(10);
+  });
+
+  it('generates the array in incoming order by default', () => {
+    const arr1 = probabilityArray(['foo', 0.3], ['bar', 0.6], ['blah', 0.1]);
+    const arr2 = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'incoming' });
+    expect(arr1).toHaveLength(100);
+    expect(arr2).toHaveLength(100);
+
+    const foo1 = arr1.slice(0, 30);
+    const bar1 = arr1.slice(30, 90);
+    const blah1 = arr1.slice(90);
+
+    const foo2 = arr2.slice(0, 30);
+    const bar2 = arr2.slice(30, 90);
+    const blah2 = arr2.slice(90);
+
+    expect(foo1).toEqual(Array(30).fill('foo'));
+    expect(bar1).toEqual(Array(60).fill('bar'));
+    expect(blah1).toEqual(Array(10).fill('blah'));
+
+    expect(foo2).toEqual(Array(30).fill('foo'));
+    expect(bar2).toEqual(Array(60).fill('bar'));
+    expect(blah2).toEqual(Array(10).fill('blah'));
+
+    expect(arr1).toEqual(arr2);
+  });
+
+  it('generates array in order from the least common item to the most common when option is set to "asc"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'asc' });
+    expect(arr).toHaveLength(100);
+
+    const blah = arr.slice(0, 10);
+    const foo = arr.slice(10, 40);
+    const bar = arr.slice(40);
+
+    expect(blah).toEqual(Array(10).fill('blah'));
+    expect(foo).toEqual(Array(30).fill('foo'));
+    expect(bar).toEqual(Array(60).fill('bar'));
+  });
+
+  it('generates array in order from the most common item to the least common when option is set to "desc"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'desc' });
+    expect(arr).toHaveLength(100);
+
+    const bar = arr.slice(0, 60);
+    const foo = arr.slice(60, 90);
+    const blah = arr.slice(90);
+
+    expect(bar).toEqual(Array(60).fill('bar'));
+    expect(foo).toEqual(Array(30).fill('foo'));
+    expect(blah).toEqual(Array(10).fill('blah'));
+  });
+
+  it('generates a array with a circular repetition of items when option is set to "circular"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'circular' });
+    expect(arr).toHaveLength(100);
+
+    const options = ['foo', 'bar', 'blah'];
+
+    for (let i = 0; i < 30; i += 1) {
+      // For the first 30 items, the items should be in the order of 'foo', 'bar', 'blah'
+      const item = arr[i];
+      const index = options.indexOf(item);
+      const indexInOptions = i % options.length;
+
+      expect(item).toEqual(options[indexInOptions]);
+    }
+
+    options.pop();
+
+    for (let i = 30; i < 70; i += 1) {
+      // For the next 40 items, the items should be in the order of 'foo', 'bar'
+      const item = arr[i];
+      const index = options.indexOf(item);
+      const indexInOptions = i % options.length;
+
+      expect(item).toEqual(options[indexInOptions]);
+    }
+
+    // All that remains in the end is just 'bar'
+    expect(arr.slice(70)).toEqual(Array(30).fill('bar'));
+  });
+
+  it('generates an array with random distribution of the items when option is set to "random"', () => {
+    const arr1 = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'random' });
+    const arr2 = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'random' });
+    const arr3 = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1]], { sortOrder: 'random' });
+    expect(arr1).toHaveLength(100);
+    expect(arr2).toHaveLength(100);
+    expect(arr3).toHaveLength(100);
+
+    const foo1 = arr1.filter((item) => item === 'foo');
+    const bar1 = arr1.filter((item) => item === 'bar');
+    const blah1 = arr1.filter((item) => item === 'blah');
+
+    const foo2 = arr2.filter((item) => item === 'foo');
+    const bar2 = arr2.filter((item) => item === 'bar');
+    const blah2 = arr2.filter((item) => item === 'blah');
+
+    const foo3 = arr3.filter((item) => item === 'foo');
+    const bar3 = arr3.filter((item) => item === 'bar');
+    const blah3 = arr3.filter((item) => item === 'blah');
+
+    expect(foo1).toHaveLength(30);
+    expect(bar1).toHaveLength(60);
+    expect(blah1).toHaveLength(10);
+
+    expect(foo2).toHaveLength(30);
+    expect(bar2).toHaveLength(60);
+    expect(blah2).toHaveLength(10);
+
+    expect(foo3).toHaveLength(30);
+    expect(bar3).toHaveLength(60);
+    expect(blah3).toHaveLength(10);
+    
+    expect(arr1).not.toEqual(arr2);
+    expect(arr1).not.toEqual(arr3);
+    expect(arr2).not.toEqual(arr3);
+  });
+
+  it('reduces the size of the biggest array if option in case they all exceed 100 is set to "biggest"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.1], ['boom', 0.3]], { cutoff: 'biggest' });
+    expect(arr).toHaveLength(100);
+
+    const bar = arr.filter((item) => item === 'bar');
+
+    expect(bar).toHaveLength(30);
+  });
+
+  it('reduces the size of the smallest array if option in case they all exceed 100 is set to "smallest"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.3], ['blah', 0.2], ['boom', 0.3]], { cutoff: 'smallest' });
+    expect(arr).toHaveLength(100);
+
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(blah).toHaveLength(10);
+  })
+
+  it('normalizes the sizes of the arrays if option in case they all exceed 100 is set to "normalize"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.3]], { cutoff: 'normalize' });
+    expect(arr).toHaveLength(100);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(25);
+    expect(bar).toHaveLength(50);
+    expect(blah).toHaveLength(25);
+  })
+
+  it('retains at least cell in parts that are so small that under normalization will be eliminated', () => {
+    const arr = probabilityArray([['foo', 0.49], ['bar', 0.6], ['blah', 0.01]], { cutoff: 'normalize' });
+    expect(arr).toHaveLength(100);
+
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(44);
+    expect(bar).toHaveLength(54);
+    expect(blah).toHaveLength(1);
+  });
+
+  it('reduce the array parts as evenly as possible (numerically) when exceeding 100 and option is set to "spread"', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', 0.6], ['blah', 0.2], ['boom', 0.3]], { cutoff: 'spread' });
+    expect(arr).toHaveLength(100);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+    const boom = arr.filter((item) => item === 'boom');
+
+    expect(foo).toHaveLength(20);
+    expect(bar).toHaveLength(50);
+    expect(blah).toHaveLength(10);
+    expect(boom).toHaveLength(20);
+  });
+
+  it('retains at least one cell in parts that are so small that under spread will be eliminated', () => {
+    const arr = probabilityArray([['foo', 0.5], ['bar', 0.6], ['blah', 0.01]], { cutoff: 'spread' });
+    expect(arr).toHaveLength(100);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(44);
+    expect(bar).toHaveLength(55);
+    expect(blah).toHaveLength(1);
+  });
+
+  it('does not modify the input', () => {
+    const input: ProbabilityItem<string>[] = [['foo', 0.3], ['bar', 0.6], ['blah', 0.6]];
+    const arr = probabilityArray(...input);
+    expect(arr).toHaveLength(100);
+
+    expect(input).toEqual([['foo', 0.3], ['bar', 0.6], ['blah', 0.6]]);
+  })
+
+  it('treats negative numbers as 0', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', -0.6], ['blah', 0.1]]);
+    expect(arr).toHaveLength(40);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(30);
+    expect(bar).toHaveLength(0);
+    expect(blah).toHaveLength(10);
+  });
+
+  it('consolidates with negative numbers', () => {
+    const arr = probabilityArray([['foo', 0.3], ['bar', -0.2], ['blah', 0.1], ['bar', 0.8]]);
+    expect(arr).toHaveLength(100);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+
+    expect(foo).toHaveLength(30);
+    expect(bar).toHaveLength(60);
+    expect(blah).toHaveLength(10);
+  });
+
+  it('removes values with accumulative ratio smaller than 0 when reducing the size of the array', () => {
+    const arr = probabilityArray([['foo', 0.7], ['bar', 0.-6], ['blah', 0.4], ['bar', 0.3]]);
+    expect(arr).toHaveLength(100);
+    
+    const foo = arr.filter((item) => item === 'foo');
+    const bar = arr.filter((item) => item === 'bar');
+    const blah = arr.filter((item) => item === 'blah');
+    const boom = arr.filter((item) => item === 'boom');
+
+    expect(foo).toHaveLength(70);
+    expect(bar).toHaveLength(0);
+    expect(blah).toHaveLength(30);
   });
 
   // it('is used by the randomator for a good probability for random selection', () => {
--- src/functions/probabilityArray.ts
+++ src/functions/probabilityArray.ts
@@ -1,10 +1,182 @@
-import isEmpty from './isEmpty';
+import mapValues from 'lodash/mapValues';
+import shuffle from 'lodash/shuffle';
+
+
+export type ProbabilityItem<T> = [T, number];
+type SortOrder = 'incoming' | 'asc' | 'desc' | 'circular' | 'random';
+type Cutoff = 'last' | 'biggest' | 'smallest' | 'normalize' | 'spread';
+
+
+function copyProbabilityItems<T>(items: ProbabilityItem<T>[]): ProbabilityItem<T>[] {
+  return items.map((item) => [item[0], Math.floor(item[1] * 100)]);
+}
+
+
+function consolidateDuplicateItems<T>(inputItems: ProbabilityItem<T>[]): ProbabilityItem<T>[] {
+  const items = copyProbabilityItems(inputItems);
+  const result: ProbabilityItem<T>[] = [];
+  const seenItems = new Map<T, number>();
+
+  // First pass - consolidate probabilities
+  for (const [item, probability] of items) {
+    if (seenItems.has(item)) {
+      const index = seenItems.get(item)!;
+      const currentProb = result[index][1];
+      const newProb = Math.min(currentProb + probability, 100); // Cap at 100
+      result[index] = [item, newProb];
+    } else {
+      seenItems.set(item, result.length);
+      result.push([item, probability]);
+    }
+  }
+
+  return result;
+}
+
+// This function assumes that the total amounts to more than 100
+function normalizeToSum100(input: number[]): number[] {
+
+  const MIN = 1;
+  const total = input.reduce((sum, n) => sum + n, 0);
+  const scaled = input.map(n => (n / total) * 100);
+
+  // Apply floor and enforce minimum of 1
+  const result = scaled.map(n => Math.max(Math.floor(n), MIN));
+
+  let sum = result.reduce((s, n) => s + n, 0);
+  let deficit = 100 - sum;
+
+  // Distribute remaining units fairly (starting from largest fractional parts)
+  if (deficit > 0) {
+    const fractions = scaled.map((n, i) => ({
+      i,
+      frac: n - Math.floor(n),
+    }));
+
+    // Sort by largest fractional part
+    fractions.sort((a, b) => b.frac - a.frac);
+
+    for (let i = 0; i < deficit; i++) {
+      result[fractions[i % input.length].i]++;
+    }
+  }
+
+  return result;
+}
+
+// This function assumes that the total amounts to more than 100
+function removeEvenly(values: number[]) {
+
+  const total = values.reduce((sum, val) => sum + val, 0);
+  let excess = total - 100;
+
+  const result = [...values]; // copy to avoid mutating input
+
+  // Keep reducing until excess is gone
+  let i = 0;
+  while (excess > 0) {
+    const idx = i % result.length;
+
+    if (result[idx] > 1) {
+      result[idx] -= 1;
+      excess -= 1;
+    }
+
+    i++;
+  }
+
+  return result;
+}
+
+function cutoffExcess<T>(allocations: T[][], cutoff: Cutoff = 'last') {
+
+  function findCellThatIsThe<T>() {
+
+    switch (cutoff) {
+      case 'last':
+        return allocations[allocations.length - 1];
+      case 'biggest':
+        return allocations.find((arr) => arr.length === Math.max(...allocations.map((arr) => arr.length)));
+      case 'smallest':
+        return allocations.find((arr) => arr.length === Math.min(...allocations.map((arr) => arr.length)));
+      default:
+        return undefined;
+    }
+  }
+
+  let totalProbability = allocations.reduce((acc, curr) => acc + curr.length, 0);
+
+  if (totalProbability > 100) {
+
+    let reductionFactors: number[] | null = null;
+
+    if (cutoff === 'normalize') {
+      reductionFactors = normalizeToSum100(allocations.map((arr) => arr.length));
+    }
+
+    if (cutoff === 'spread') {
+      reductionFactors = removeEvenly(allocations.map((arr) => arr.length));
+    }
+
+    if (reductionFactors) {
+      allocations.forEach((arr, index) => {
+        arr.length = reductionFactors[index];
+      });
+      return;
+    }
+  }
+
+  while (totalProbability > 100) {
+    const toRemove = totalProbability - 100;
+    const last = findCellThatIsThe();
+
+    if (!last) break;
+    const amountToRemove = Math.min(toRemove, last.length);
+    last.length = last.length - amountToRemove;
+
+    if (last.length === 0) {
+      allocations.splice(allocations.indexOf(last), 1);
+    }
+
+    totalProbability = allocations.reduce((acc, curr) => acc + curr.length, 0);
+  }
+  
+  
+}
+
+function sortProbabilityItems<T>(items: ProbabilityItem<T>[], sortOrder?: SortOrder) {
+  
+  switch (sortOrder) {
+    case 'asc':
+      items.sort((a, b) => a[1] - b[1]);
+      break;
+    case 'desc':
+      items.sort((a, b) => b[1] - a[1]);
+      break;
+    default:
+      return;
+      
+  }
+}
+
+
+interface ProbabilityArrayOptions {
+  sortOrder?: SortOrder;
+  cutoff?: Cutoff;
+}
+
+const defaultOptions: ProbabilityArrayOptions = {
+  sortOrder: 'incoming',
+  cutoff: 'last',
+};
+
 
 /**
  * Takes a list of items of any type and ratios, and returns an array with 100 cells.
  * All items appear in the array according to the specified ratios.
  * The items need to be provided in pairs implemented via arrays.
  *
+ * @example
  * ```typescript
  * const arr = probabilityArray(['foo', 0.2], ['bar', 0.3], ['boom', 0.5]);
  *
@@ -16,44 +188,157 @@ import isEmpty from './isEmpty';
  * expect(bar).toHaveLength(30);
  * expect(boom).toHaveLength(50);
  * ```
+ *
  * @param args The arguments to place in the array in the desired ratio `[item, amount]`
  * @returns Array of size 100, that has all items - each in the specified quantity
  */
-export default function probabilityArray<T>(...args: [T, number][]): T[] {
+export default function probabilityArray<T>(...args: ProbabilityItem<T>[]): T[];
+
+/**
+ * Takes a list of items of any type and ratios and settings definitions, and returns an array with 100 cells, constructed according to the settings.
+ * All items appear in the array according to the specified ratios.
+ * The items need to be provided in pairs implemented via arrays.
+ *
+ * @example
+  * ```typescript
+ * const arr = probabilityArray([['foo', 0.2], ['bar', 0.3], ['boom', 0.5]], { sortOrder: 'incoming', cutoff: 'last' });
+ *
+ * const foo = arr.filter((item) => item === 'foo');
+ * const bar = arr.filter((item) => item === 'bar');
+ * const boom = arr.filter((item) => item === 'boom');
+ *
+ * expect(foo).toHaveLength(20);
+ * expect(bar).toHaveLength(30);
+ * expect(boom).toHaveLength(50);
+ * ```
+ *
+ * Optional Modifiers:
+ * ===================
+ * `sortOrder`
+ * -------------------
+ * *defaults to* `'incoming'`
+ * Dictates the order of the items in the array according to one of five options:
+ * - `'incoming'` - the items will be sorted in the order they were given in the arguments.
+ * - `'asc'` - the items will be sorted in ascending order of their ratios—from the least common to the most common.
+ * - `'desc'` - the items will be sorted in descending order of their ratios—from the most common to the least common.
+ * - `'circular'` - the items will be sorted in a circular repetitive order, starting from the first item.
+ * - `'random'` - the items will be sorted in a random order.
+ *
+ * @example
+ * ```typescript
+ * const incoming = probabilityArray([['A', 0.3], ['B', 0.2], ['C', 0.5]], { sortOrder: 'incoming' });
+ * const asc = probabilityArray([['A', 0.3], ['B', 0.2], ['C', 0.5]], { sortOrder: 'asc' });
+ * const desc = probabilityArray([['A', 0.3], ['B', 0.2], ['C', 0.5]], { sortOrder: 'desc' });
+ * const circular = probabilityArray([['A', 0.3], ['B', 0.2], ['C', 0.5]], { sortOrder: 'circular' });
+ * const random = probabilityArray([['A', 0.3], ['B', 0.2], ['C', 0.5]], { sortOrder: 'random' });
+ *
+ * console.log(incoming); // ['A',...,'A', 'B',...,'B', 'C',...,'C']
+ * console.log(asc);      // ['B',...,'B', 'A',...,'A', 'C',...,'C']
+ * console.log(desc);     // ['C',...,'C', 'A',...,'A', 'B',...,'B']
+ * console.log(circular); // ['A', 'B', 'C', 'A', 'B', 'C', 'A', 'B', 'C', ...]
+ * console.log(random);   // ['C', 'B', 'A', 'A', 'C', 'C', 'C', 'B', 'C', ...]
+ * ```
+ *
+ * `cutoff`
+ * -------------------
+ * *defaults to* `'last'`
+ * Dictates the way to handle the items that exceed the 100% ratio.
+ * - `'last'` - the last items in the argument will be reduced or removed.
+ * - `'biggest'` - the items with the highest ratios will be reduced or removed.
+ * - `'smallest'` - the items with the lowest ratios will be reduced or removed.
+ * - `'normalize'` - all items will be reduced in order to reach a total of 100 while roughly preserving the ratios between them (with minimum of 1 for each item)
+ * - `'spread'` - the items will be reduced by roughly the same amount, so that the total is 100.
+ *
+ * @example
+ * ```typescript
+ * const last = probabilityArray([['A', 0.2], ['B', 0.5], ['C', 0.4], ['D', 0.3]], { cutoff: 'last' });
+ * const biggest = probabilityArray([['A', 0.2], ['B', 0.5], ['C', 0.4], ['D', 0.3]], { cutoff: 'biggest' });
+ * const smallest = probabilityArray([['A', 0.2], ['B', 0.5], ['C', 0.4], ['D', 0.3]], { cutoff: 'smallest' });
+ * const normalize = probabilityArray([['A', 0.2], ['B', 0.5], ['C', 0.4], ['D', 0.3]], { cutoff: 'normalize' });
+ * const spread = probabilityArray([['A', 0.2], ['B', 0.5], ['C', 0.4], ['D', 0.3]], { cutoff: 'spread' });
+ *
+ * expect(last.filter((item) => item === 'C')).toHaveLength(20);
+ * expect(last.filter((item) => item === 'D')).toHaveLength(0);
+ * 
+ * expect(biggest.filter((item) => item === 'B')).toHaveLength(10);
+ * 
+ * expect(smallest.filter((item) => item === 'A')).toHaveLength(0);
+ * expect(smallest.filter((item) => item === 'D')).toHaveLength(10);
+ * 
+ * expect(normalize.filter((item) => item === 'A')).toHaveLength(14);
+ * expect(normalize.filter((item) => item === 'B')).toHaveLength(36);
+ * expect(normalize.filter((item) => item === 'C')).toHaveLength(29);
+ * expect(normalize.filter((item) => item === 'D')).toHaveLength(21);
+ * 
+ * expect(spread.filter((item) => item === 'A')).toHaveLength(10);
+ * expect(spread.filter((item) => item === 'B')).toHaveLength(40);
+ * expect(spread.filter((item) => item === 'C')).toHaveLength(30);
+ * expect(spread.filter((item) => item === 'D')).toHaveLength(20);
+ * ```
+ *
+ * @param ratios An array that contains items and their ratios `[item, amount]`
+ * @param options Options to modify the behavior of the function
+ * @returns Array of size 100, that has all items - each in the specified quantity
+ */
+export default function probabilityArray<T>(ratios: ProbabilityItem<T>[], options?: ProbabilityArrayOptions): T[];
+
+export default function probabilityArray<T>(...args: any[]): T[] {
+  
+  const options: ProbabilityArrayOptions =  args.length < 2 || Array.isArray(args[1]) ? defaultOptions : mapValues(defaultOptions, (value, key) => args[1][key] || value);
+
+  const ratios: ProbabilityItem<T>[] = Array.isArray(args[0]) && typeof args[0][0] !== 'undefined' && typeof args[0][1] === 'number'
+    ? consolidateDuplicateItems(args)
+    : consolidateDuplicateItems(args[0]);
+
+  if (ratios.length > 100) {
+    // More than 100 elements were given so even if each has 0.01 probability, it will always be more than 100
+    // We're sim;ly cutting off the last given items regardless of given options:
+    ratios.length = 100;
+  }
+
+  sortProbabilityItems(ratios, options.sortOrder);
+
+  const allocations: T[][] = [];
 
   let totalSum = 0;
-  const arrays: T[][] = [];
-
-  let i = 0;
-  while (totalSum < 100 && i < args.length) {
-    const item = args[i][0];
-    let amount = Math.floor(args[i][1] * 100);
+  for (const ratio of ratios) {
+    const item = ratio[0];
+    let amount = ratio[1];
 
     totalSum += amount;
 
-    if (totalSum > 100) {
-      amount -= totalSum - 100;
-    }
-    arrays.push(Array.from({ length: amount }, () => item));
-
-    i += 1;
+    allocations.push(Array.from({ length: amount }, () => item));
   }
 
-  const retArr: T[] = [];
-  while (!isEmpty(arrays)) {
-    if (arrays.length > 1) {
-      arrays.forEach((arr, index, thisArr) => {
-        retArr.push(arr.pop()!);
+  cutoffExcess(allocations, options.cutoff);
+  
+  let result: T[];
 
-        if (arr.length === 0) {
-          thisArr.splice(index, 1);
+  switch (options.sortOrder) {
+    case 'circular':
+      result = [];
+      let circulator = 0;
+
+      while (allocations.length > 0) {
+        result.push(allocations[circulator].pop()!);
+
+        if (allocations[circulator].length === 0) {
+          allocations.splice(circulator, 1);
+        } else {
+          circulator += 1;
         }
-      });
-    } else {
-      retArr.push(...arrays[0]);
+
+        if (circulator >= allocations.length) {
+          circulator = 0;
+        }
+      }
       break;
-    }
+    case 'random':
+      result = shuffle(allocations.reduce((acc, curr) => acc.concat(curr), [] as T[]));
+      break;
+    default:
+      result = allocations.reduce((acc, curr) => acc.concat(curr), [] as T[]);
   }
 
-  return retArr;
+  return result;
 }
